8518b91fa8331c96356a41b8a350b540b2972e56
ALOKKUMARSAW <aloksaw512@gmail.com>
2022-09-04 18:40:02 +0530

    add file

---
+++ Untitled-3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int num,index=0;
+   printf("Enteranumber\n");
+   scanf("%d",&num);
+   do
+   {}
+       printf("%d",index);
+        index=index+1;
+    while(index<num);
+    return0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
